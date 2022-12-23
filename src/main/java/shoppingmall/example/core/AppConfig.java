024e38e11d3cab9dfb36f8b8e232eae2e97a16fb
sdsr <dlxodnr1212@gmail.com>
2022-12-23 17:13:59 +0900

    fix: refactoring AppConfig

--- src/main/java/shoppingmall/example/core/AppConfig.java
+++ src/main/java/shoppingmall/example/core/AppConfig.java
@@ -1,10 +1,8 @@
 package shoppingmall.example.core;
 
+import shoppingmall.example.core.discount.DiscountPolicy;
 import shoppingmall.example.core.discount.FixDiscountPolicy;
-import shoppingmall.example.core.member.Member;
-import shoppingmall.example.core.member.MemberService;
-import shoppingmall.example.core.member.MemberServiceImpl;
-import shoppingmall.example.core.member.MemoryMemberRepository;
+import shoppingmall.example.core.member.*;
 import shoppingmall.example.core.order.Order;
 import shoppingmall.example.core.order.OrderService;
 import shoppingmall.example.core.order.OrderServiceImpl;
@@ -12,10 +10,18 @@ import shoppingmall.example.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
