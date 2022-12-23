24f6e568fe4c28247ce6cb7b4fd6ee9d5846ca47
sdsr <dlxodnr1212@gmail.com>
2022-12-23 14:29:47 +0900

    feat: RateDiscountPolicy

---
+++ src/main/java/shoppingmall/example/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package shoppingmall.example.core.discount;
+
+import shoppingmall.example.core.member.Grade;
+import shoppingmall.example.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
