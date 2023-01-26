8b5c70a03a8dde49ab33188931c3eb90196806a2
sdsr <dlxodnr1212@gmail.com>
2023-01-26 17:32:27 +0900

    Test: @Configuration and singleton

---
+++ src/test/java/shoppingmall/example/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,35 @@
+package shoppingmall.example.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import shoppingmall.example.core.AppConfig;
+import shoppingmall.example.core.member.MemberRepository;
+import shoppingmall.example.core.member.MemberServiceImpl;
+import shoppingmall.example.core.order.OrderServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+    }
+}
