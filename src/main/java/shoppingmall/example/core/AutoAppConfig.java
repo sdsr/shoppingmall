e0cc70ba2f3e6268d29d032cf9d58319967ac963
sdsr <dlxodnr1212@gmail.com>
2023-01-27 15:51:40 +0900

    Test: component scan location and target

--- src/main/java/shoppingmall/example/core/AutoAppConfig.java
+++ src/main/java/shoppingmall/example/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "shoppingmall.example.core.member",
+        basePackageClasses = AutoAppConfig.class, // 지정 안하면 default shoppingmall.example.core 부터 시작해서 하위 다 찾음
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
