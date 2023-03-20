a9b9b5cc7f1dc4c22a10c30c75aa734abfeab580
깃sdsr <dlxodnr1212@gmail.com>
2023-03-20 17:15:34 +0900

    feat: demo logger "scope and provider"

--- src/main/java/shoppingmall/example/core/web/LogDemoController.java
+++ src/main/java/shoppingmall/example/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package shoppingmall.example.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,17 @@ import shoppingmall.example.core.common.MyLogger;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        //Thread.sleep(1000);
         logDemoService.logic("testID");
         return "OK";
     }
