d0ba9245a40ff2b8ab33e1f50a9ab6dadb0f85e0
깃sdsr <dlxodnr1212@gmail.com>
2023-03-20 17:03:25 +0900

    feat: http logger

---
+++ src/main/java/shoppingmall/example/core/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package shoppingmall.example.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shoppingmall.example.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+
+    }
+}
